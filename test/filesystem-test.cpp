0f98819519b3799d7c9dbf816c75441739ad54b9
Huu Nguyen <huu@prismskylabs.com>
2015-12-16 13:31:52 -0500

    Add tests for buffer directory constructor

--- test/filesystem-test.cpp
+++ test/filesystem-test.cpp
@@ -13,3 +13,67 @@
 TEST_F(FilesystemFixture, EmptyFSTest) {
     EXPECT_FALSE(fs::exists(buffer_path_));
 }
+
+TEST_F(FilesystemFixture, ConstructFSTest) {
+    EXPECT_FALSE(fs::exists(buffer_path_));
+    prism::indexed::Filesystem filesystem{"prism_indexed_buffer"};
+    EXPECT_TRUE(fs::exists(buffer_path_));
+}
+
+TEST_F(FilesystemFixture, ConstructFSNoDestructTest) {
+    EXPECT_FALSE(fs::exists(buffer_path_));
+    {
+        prism::indexed::Filesystem filesystem{"prism_indexed_buffer"};
+        EXPECT_TRUE(fs::exists(buffer_path_));
+    }
+    EXPECT_TRUE(fs::exists(buffer_path_));
+}
+
+TEST_F(FilesystemFixture, ConstructFSMultipleTest) {
+    EXPECT_FALSE(fs::exists(buffer_path_));
+    {
+        prism::indexed::Filesystem filesystem{"prism_indexed_buffer"};
+        EXPECT_TRUE(fs::exists(buffer_path_));
+    }
+    {
+        prism::indexed::Filesystem filesystem{"prism_indexed_buffer"};
+        EXPECT_TRUE(fs::exists(buffer_path_));
+    }
+    EXPECT_TRUE(fs::exists(buffer_path_));
+}
+
+TEST_F(FilesystemFixture, ConstructThrowTest) {
+    bool thrown = false;
+    try {
+        prism::indexed::Filesystem filesystem{""};
+    } catch (const prism::indexed::FilesystemException& e) {
+        thrown = true;
+        EXPECT_EQ(std::string{"Cannot initialize indexed Filesystem with an empty buffer path"},
+                  std::string{e.what()});
+    }
+    EXPECT_TRUE(thrown);
+}
+
+TEST_F(FilesystemFixture, ConstructCurrentThrowTest) {
+    bool thrown = false;
+    try {
+        prism::indexed::Filesystem filesystem{"."};
+    } catch (const prism::indexed::FilesystemException& e) {
+        thrown = true;
+        EXPECT_EQ(std::string{"Filesystem must be initialized within a valid parent directory"},
+                  std::string{e.what()});
+    }
+    EXPECT_TRUE(thrown);
+}
+
+TEST_F(FilesystemFixture, ConstructParentThrowTest) {
+    bool thrown = false;
+    try {
+        prism::indexed::Filesystem filesystem{".."};
+    } catch (const prism::indexed::FilesystemException& e) {
+        thrown = true;
+        EXPECT_EQ(std::string{"Filesystem must be initialized within a valid parent directory"},
+                  std::string{e.what()});
+    }
+    EXPECT_TRUE(thrown);
+}
