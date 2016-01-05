80c7b27a9e4ffd41f77456ec15a168c82a86513c
Huu Nguyen <huu@prismskylabs.com>
2016-01-05 14:54:40 -0500

    Use comparison with explicit string object instead of cstring literal

--- test/filesystem-fixture.h
+++ test/filesystem-fixture.h
@@ -19,11 +19,11 @@ class FilesystemFixture : public ::testing::Test {
 
     int numberOfFiles() {
         fs::directory_iterator begin(buffer_path_), end;
-        return std::count_if(begin, end,
-                [] (const fs::directory_entry& f) {
-                    return !(fs::is_directory(f.path()) ||
-                             f.path().filename().native().substr(0, 18) == "prism_indexed_data");
-                });
+        return std::count_if(begin, end, [](const fs::directory_entry& f) {
+            return !(fs::is_directory(f.path()) ||
+                     f.path().filename().native().substr(0, 18) ==
+                             std::string{"prism_indexed_data"});
+        });
     }
 
     fs::path buffer_path_;
