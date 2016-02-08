a994f0c787f04eccf6f319ddc75fcf769278752e
Huu Nguyen <huu@prismskylabs.com>
2016-02-08 10:48:00 -0800

    Remove unnecessary code branch

--- src/chrono-snap.cpp
+++ src/chrono-snap.cpp
@@ -9,16 +9,9 @@ namespace utility {
 
 unsigned long long SnapToMinute(const std::chrono::system_clock::time_point& time_point) {
     auto milliseconds = std::chrono::time_point_cast<std::chrono::milliseconds>(time_point);
-    if (milliseconds.time_since_epoch().count() % 60000 < 30000) {
-        return std::chrono::time_point_cast<std::chrono::minutes>(time_point)
-                .time_since_epoch()
-                .count();
-    } else {
-        return std::chrono::time_point_cast<std::chrono::minutes>(time_point +
-                                                                  std::chrono::seconds(30))
-                .time_since_epoch()
-                .count();
-    }
+    return std::chrono::time_point_cast<std::chrono::minutes>(time_point + std::chrono::seconds(30))
+            .time_since_epoch()
+            .count();
 }
 
 } // namespace utility
