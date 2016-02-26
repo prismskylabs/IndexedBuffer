06d4defa8a22a57b577731c25ce1ee5c30379c33
Huu Nguyen <huu@prismskylabs.com>
2016-02-26 11:41:19 -0800

    Add GetExistingFilepath method to Filesystem interface

--- include/indexed/filesystem.h
+++ include/indexed/filesystem.h
@@ -19,6 +19,7 @@ class Filesystem {
     bool AboveQuota() const;
     bool Delete(const std::string& filename);
     std::string GetBufferDirectory() const;
+    std::string GetExistingFilepath(const std::string& filename) const;
     std::string GetFilepath(const std::string& filename) const;
     bool Move(const std::string& filepath_move_from, const std::string& filename_move_to);
 
